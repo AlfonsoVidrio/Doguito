b0795653de5a7ce97b6bcb7b3d532c4d6d8ce846
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-08-24 18:45:26 -0600

    Set up initial routing with react-router-dom including Home, About, and 404 page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css';
+import { Home } from './pages/Home.jsx';
+import { About } from './pages/About.jsx';
+import { Page404 } from './pages/Page404.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header.jsx';
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Router>
+        <Routes >
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='*' element={<Page404 />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
